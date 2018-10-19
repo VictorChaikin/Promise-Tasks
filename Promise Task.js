08d97d457418850e010940c2f0cbcdd40ad94450
ChaikinVictor <https://github.com/VictorChaikin>
2018-10-19 22:54:06 +0300

    Third Version

--- Promise Task.js
+++ Promise Task.js
@@ -1,17 +1,25 @@
 'use strict';
 
-function A(a,b,c){
+function A(){
 	//constructor(a,b,c){
-		let _a = a;
-		let _b = b;
-		let _c = c;
+		let _a;
+		let _b;
+		let _c;
+		let int = 3;
 	//}
+    this._getInt= function () {
+        return int;
+
+    };
+    let setInt = function(val){
+        int = val;
+    };
 	//let self = this;
 	let _setA = function(){
 		return new Promise((resolve)=>{
             let interval = Math.ceil(Math.random()*80+50);
-             console.log("A = "+interval);
-            setTimeout(()=>{_a = Math.random() * 10000 + 10},interval);
+             console.log("Interval A = "+interval);
+            setTimeout(()=>{_a = (Math.random() * 10000 + 10)},interval);
             resolve(interval);
 		});
 	};
@@ -19,31 +27,27 @@ function A(a,b,c){
     let _setB = function(){
     	return new Promise((resolve)=>{
             let interval = Math.ceil(Math.random()*190+30);
-            console.log("B = "+interval);
-            setTimeout(()=>{_b = Date.now()/3000000},interval);
+            console.log("Interval B = "+interval);
+            setTimeout(()=>{_b = (Date.now()/3000000)},interval);
             resolve(interval) ;
 		});
 	};
 	 let _setC = function(value){
              let interval = Math.ceil(Math.random()*290+10);
-            console.log("C = "+interval);
+            console.log("Interval C = "+interval);
             setTimeout(()=>{_c = value*10},interval);
             return interval;
 	 };
 	  this.process = function(){
-	  	    Promise.all([_setA(),_setB()]).then(value => {
+	  	    return Promise.all([_setA(),_setB()]).then(value => {
                 let interval = value.reduce((int,current)=>int+current,0);
                 let a = _setC(Math.ceil(interval/2));
+                //console.log("C interval ="+a);
                 let finalInterval = Math.ceil((a+interval)/3);
-                console.log("Fin = "+finalInterval);
-
-                // resolve(finalInterval);
-                //if(finalInterval<100)
+                //console.log("Fin = "+finalInterval);
+                setInt(finalInterval);
                 return finalInterval;
-                //else
-                    //process(counter++);
-	  	    });
-
+	  	    }).then(value =>  value);
 	 };
 	 this.show = function(){
 		console.log("A = "+_a);
@@ -52,12 +56,31 @@ function A(a,b,c){
 	 }
 }
 
-let c = new A(0,3,3);
-let counter =0;
-let a;
-do{
-     a = c.process();
-     console.log("A = "+a);
-    counter++;
-}while(a>100 && counter<10000) ;
+let c = new A();
+const intervalTimer = setInterval(()=>c.show(),500);
+
+function start() {
+    let counter =0;
+    repeat();
+    function repeat(){
+        counter++;
+        c.process().then(result=> {
+            if (result > 100) {
+                if (counter < 10000) {
+                    repeat();
+                }
+                else {
+                    c.show();
+                    clearInterval(intervalTimer);
+                }
+            }
+            else {
+                c.show();
+                console.log("RESULT = "+result);
+                clearInterval(intervalTimer);
+            }
+        });
+    }
+}
+start();
 //setInterval(c.show,500);
